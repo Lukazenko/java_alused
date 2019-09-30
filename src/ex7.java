d82658be9a9bd37e156febbc6191c0693de26cdd
Lukazenko <veikko.kiis@khk.ee>
2019-09-30 11:16:06 +0300

    exercise 7: Multiplication

---
+++ src/ex7.java
@@ -0,0 +1,9 @@
+public class ex7 {
+    public static void main(String[] args){
+        int one = 12345;
+        int two = 987;
+        int sum = one * two;
+
+        System.out.println(one + " * " + two + " = " + sum);
+    }
+}
