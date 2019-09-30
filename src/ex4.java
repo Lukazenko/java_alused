428828e68fd72b0ad1ac23a25d1c09536f1400b2
Lukazenko <veikko.kiis@khk.ee>
2019-09-30 10:12:08 +0300

    exercise 4: Variable data types are immutable

---
+++ src/ex4.java
@@ -0,0 +1,13 @@
+public class ex4 {
+    public static void main(String[] args) {
+       int chickens = 9000;
+       double bacon = 0.1;
+       String tractor = "Zetor";
+
+       System.out.println("Chickens: \n" + chickens);
+       System.out.println("Bacon (kg): \n" + bacon);
+       System.out.println("A tractor: \n" + tractor);
+
+       System.out.println("In a nutshell: \n" + chickens + "\n" + bacon + "\n" + tractor);
+    }
+}
