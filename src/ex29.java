c4ba0c503290eeda46ff39e2af9418cf54ac7b5c
Lukazenko <veikko.kiis@khk.ee>
2019-10-07 13:18:35 +0300

    exercise 29: Even numbers

---
+++ src/ex29.java
@@ -0,0 +1,12 @@
+public class ex29 {
+    public static void main(String[] args){
+
+        int number = 2;
+        while(number <= 100){
+
+            System.out.println(number);
+            number = number + 2;
+
+        }
+    }
+}
