3ce4b78df334e45583e22f0d66ef9942e037bb82
Lukazenko <veikko.kiis@khk.ee>
2019-10-14 14:02:46 +0300

    ex37: Methods, Printing out text

---
+++ src/ex37.java
@@ -0,0 +1,9 @@
+public class ex37 {
+    public static void main(String[] args){
+        printText();
+    }
+
+    private static void printText() {
+        System.out.print("In the beginning there were the swamp, the hoe and Java.");
+    }
+}
