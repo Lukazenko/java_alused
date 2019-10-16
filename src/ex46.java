c6a23e8cc68b43c5007645f4587402a0dfc62349
Lukazenko <veikko.kiis@khk.ee>
2019-10-16 15:10:56 +0300

    ex46: Average of given numbers

---
+++ src/ex46.java
@@ -0,0 +1,13 @@
+public class ex46 {
+    private static double average(int number1, int number2, int number3, int number4) {
+        int sum = number1 + number2 + number3 + number4;
+        return (double)sum / 4;
+    }
+
+    public static void main(String[] args) {
+        double answer = average(4, 3, 6, 1);
+        System.out.println("average: " + answer);
+    }
+
+
+}
