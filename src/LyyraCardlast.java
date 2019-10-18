40c510124363e65adcc0e859b2e05806f4578acb
Lukazenko <veikko.kiis@khk.ee>
2019-10-18 17:45:13 +0300

    ex77: Multiple cards

---
+++ src/LyyraCardlast.java
@@ -0,0 +1,36 @@
+public class LyyraCardlast {
+    private double balance;
+    private double originalbalance;
+
+    LyyraCardlast(double balanceAtStart) {
+        this.balance = balanceAtStart;
+        this.originalbalance = balanceAtStart;
+    }
+
+    public String toString() {
+        return "The card has " + this.balance + " euros";
+    }
+    void payEconomical() {
+        if(this.balance - 2.50 > 0){
+            this.balance = this.balance - 2.50;
+        }
+
+    }
+
+    void payGourmet() {
+        if(this.balance - 4.00 > 0){
+            this.balance = this.balance - 4.00;
+        }
+
+    }
+
+    void loadMoney(double amount) {
+        this.balance = this.balance + amount;
+        if(this.balance > 150){
+            this.balance = 150;
+        } else if(this.balance < 0){
+            this.balance = this.originalbalance;
+        }
+
+    }
+}
