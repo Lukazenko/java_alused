1092bbbabad0efc2a703cf363318ec416b2978be
Lukazenko <veikko.kiis@khk.ee>
2019-10-18 17:27:27 +0300

    ex77: Balance nonnegative

---
+++ src/ex77c.java
@@ -0,0 +1,12 @@
+public class ex77c {
+    public static void main(String[] args) {
+        LyyraCardc card = new LyyraCardc(5);
+        System.out.println(card);
+
+        card.payGourmet();
+        System.out.println(card);
+
+        card.payGourmet();
+        System.out.println(card);
+    }
+}
