7a45ff5e9871450f62d6af8c2eda236ff2833c1c
Lukazenko <veikko.kiis@khk.ee>
2019-10-18 16:34:00 +0300

    ex75a: Implementing method decrease()

---
+++ src/ex75a.java
@@ -0,0 +1,15 @@
+public class ex75a {
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+        counter.decrease();
+        counter.printValue();
+
+
+    }
+}
