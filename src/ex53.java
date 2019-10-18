bab14f4157965d1f821581eef94622c261926468
Lukazenko <veikko.kiis@khk.ee>
2019-10-18 09:39:08 +0300

    ex53: The first part

--- src/ex53.java
+++ src/ex53.java
@@ -4,6 +4,7 @@ public class ex53 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Type a word: ");
+
         String word = reader.nextLine();
         System.out.print("Length of the first part: ");
         int number = Integer.parseInt(reader.nextLine());
