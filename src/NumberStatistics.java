cc67f354246187a7ad6fb1fb60d0db3f0914cda4
Lukazenko <veikko.kiis@khk.ee>
2019-10-28 13:13:48 +0200

    ex79a: Amount of numbers

---
+++ src/NumberStatistics.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+class NumberStatistics {
+    private int amountOfNumbers;
+
+    NumberStatistics() {
+        // initialize here the object variable amountOfNumbers
+        this.amountOfNumbers = 0;
+    }
+
+    void addNumber(int number) {
+
+        amountOfNumbers += 1;
+
+
+
+    }
+
+    int amountOfNumbers() {
+
+
+        return amountOfNumbers;
+
+    }
+
+}
---
+++ src/ex79a.java
@@ -0,0 +1,15 @@
+public class ex79a {
+    public static void main(String[] args) {
+
+            NumberStatistics stats = new NumberStatistics();
+            stats.addNumber(3);
+            stats.addNumber(5);
+            stats.addNumber(1);
+            stats.addNumber(2);
+            System.out.println("Amount: " + stats.amountOfNumbers());
+
+
+
+    }
+
+}
