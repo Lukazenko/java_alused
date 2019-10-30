a613154c94f92f3ec2f78280a7da9e2a487d8c88
Lukazenko <veikko.kiis@khk.ee>
2019-10-30 13:31:39 +0200

    ex86a: The "stupid" Lyyra card

---
+++ src/LyyraCardex86.java
@@ -0,0 +1,27 @@
+public class LyyraCardex86 {
+    private double balance;
+
+    LyyraCardex86(double balance) {
+        this.balance = balance;
+    }
+
+    double balance() {
+        return this.balance;
+    }
+
+    public void loadMoney(double amount) {
+        this.balance += amount;
+    }
+
+    boolean pay(double amount){
+        // the method checks if the balance of the card is at least the amount given as parameter
+        if(amount <= balance){
+            this.balance = this.balance - amount;
+            return true;
+        } else {
+            return false;
+        }
+        // if not, the method returns false meaning that the card could not be used for the payment
+        // if the balance is enough, the given amount is taken from the balance and true is returned
+    }
+}
