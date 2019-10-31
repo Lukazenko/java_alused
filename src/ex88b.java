bb55ed761757729a7b2e2b44140fe9e9df03d642
Lukazenko <veikko.kiis@khk.ee>
2019-10-31 13:08:24 +0200

    ex88b: List of students

---
+++ src/ex88b.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex88b {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> list = new ArrayList<Student>();
+
+        while(true){
+
+            System.out.print("name: ");
+            String name = reader.nextLine();
+            if(!(name.equals(""))){
+                System.out.print("studentnumber: ");
+                String studentnumber = reader.nextLine();
+
+                list.add(new Student(name, studentnumber));
+
+            } else {
+                for (Student asd : list){
+                    System.out.println(asd);
+                }
+
+                break;
+            }
+
+
+
+
+        }
+
+
+    }
+}
