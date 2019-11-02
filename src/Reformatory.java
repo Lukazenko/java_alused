ea9d95751001d16a2c68d69f35c943618503977c
Lukazenko <veikko.kiis@khk.ee>
2019-11-02 20:06:17 +0200

    ex85c: Number of times a weight has been measured

--- src/Reformatory.java
+++ src/Reformatory.java
@@ -1,6 +1,11 @@
 class Reformatory {
+    private int call = 0;
+
+
     int weight(Person person) {
         // returns the weight of the parameter
+
+        call += 1;
 
 
         return person.getWeight();
@@ -9,5 +14,11 @@ class Reformatory {
     void feed(Person person){
         int feed = person.getWeight() + 1;
         person.setWeight(feed);
+    }
+
+    int totalWeightsMeasured(){
+
+
+        return call;
     }
 }
