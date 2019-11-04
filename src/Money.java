375261cadcaf673447756a61efc0525e1026a655
Lukazenko <veikko.kiis@khk.ee>
2019-11-04 15:58:17 +0200

    ex95a: Plus and Money class

---
+++ src/Money.java
@@ -0,0 +1,39 @@
+public class Money {
+    private final int euros;
+    private final int cents;
+
+    Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros += cents / 100;
+            cents %= 100;
+        }
+
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    private int euros(){
+        return euros;
+    }
+
+    public int cents(){
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+
+    Money plus(Money added){
+        int eur = euros() + added.euros;
+        int cent = cents() + added.cents;
+        return new Money(eur, cent);
+
+    }
+}
