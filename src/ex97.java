74b116c6fbe8073480595c274cf83d1a55e81141
Lukazenko <veikko.kiis@khk.ee>
2019-11-04 16:39:32 +0200

    ex97: Elegant printing of an array

---
+++ src/ex97.java
@@ -0,0 +1,20 @@
+public class ex97 {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+    private static void printElegantly(int[] array) {
+        int num = array.length;
+        int i = 1;
+        for( int number : array) {
+            if(i == num){
+                System.out.print(number);
+            } else {
+                System.out.print(number + ", ");
+            }
+
+            i++;
+        }
+    }
+}
