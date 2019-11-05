51d1a8ef8e113a9b72a718098c9cc43cb0f4bd29
Lukazenko <veikko.kiis@khk.ee>
2019-11-05 12:57:08 +0200

    ex101c: Search functionality

--- src/Library.java
+++ src/Library.java
@@ -21,4 +21,47 @@ class Library {
 
     }
 
+
+    ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        // iterate the list of books and add all the matching books to the list found
+        for(Book asd : lib){
+            if(asd.title().contains(title)){
+                found.add(asd);
+            }
+        }
+
+
+        return found;
+    }
+
+    ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        // iterate the list of books and add all the matching books to the list found
+        for(Book asd : lib){
+            if(asd.publisher().contains(publisher)){
+                found.add(asd);
+            }
+        }
+
+
+        return found;
+    }
+
+    ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        // iterate the list of books and add all the matching books to the list found
+        for(Book asd : lib){
+
+            if(asd.year() == year){
+                found.add(asd);
+            }
+        }
+
+
+        return found;
+    }
 }
