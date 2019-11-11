3d689e847b702db0ea263de11182a4c052550519
Lukazenko <veikko.kiis@khk.ee>
2019-11-11 15:56:27 +0200

    ex104d: Swapping values

---
+++ src/ex104d.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class ex104d {
+    public static void main(String[] args) {
+        int[] values = {3, 2, 5, 4, 8};
+
+        System.out.println( Arrays.toString(values) );
+
+        swap(values, 1, 0);
+        System.out.println( Arrays.toString(values) );
+
+        swap(values, 0, 3);
+        System.out.println( Arrays.toString(values) );
+
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int one = array[index1];
+        int two = array[index2];
+
+        for(int i = 0; i < array.length; i++){
+            if(i == index1){
+                array[index1] = two;
+            } else if(i == index2){
+                array[index2] = one;
+            }
+
+        }
+
+
+    }
+}
