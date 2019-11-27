f7d5d6de6fff77e8b61543830354e17aeebf8017
Lukazenko <veikko.kiis@khk.ee>
2019-11-27 13:52:46 +0200

    ex105a: Is greater than

---
+++ src/GuessingGame.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    private Scanner reader;
+
+    GuessingGame() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(upperLimit, lowerLimit);
+
+        // write the game logic here
+    }
+
+    // implement here the methods isGreaterThan and average
+
+    boolean isGreaterThan(int value){
+        System.out.println("Is your number greater than " + value + "? (y/n)");
+
+        String vastus = reader.nextLine();
+
+        return vastus.equals("y");
+
+
+    }
+
+    private void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+
+        System.out.println("I promise you that I can guess the number you are thinking of with " + maxQuestions + " questions.");
+        System.out.println("");
+        System.out.println("Next I'll present you with a series of questions. Answer them honestly.");
+        System.out.println("");
+    }
+
+    // a helper method:
+    private static int howManyTimesHalvable(int number) {
+        // we create a base two logarithm  of the given value
+        // Below we swap the base number to base two logarithms!
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+
+
+}
---
+++ src/ex105a.java
@@ -0,0 +1,10 @@
+
+
+public class ex105a {
+    public static void main(String[] args) {
+
+        GuessingGame game = new GuessingGame();
+
+        System.out.println(game.isGreaterThan(32));
+    }
+}
