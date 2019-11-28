a1d1b87f450a170b10a0dc31b8b7c17d126474b0
Lukazenko <veikko.kiis@khk.ee>
2019-11-28 17:10:16 +0200

    part 2 ex4.6: Container

---
+++ src2/Container.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Container {
+    private int maxWeight;
+    ArrayList<Suitcase> container;
+
+    private int currentWeight;
+
+    Container(int max){
+        this.maxWeight = max;
+        this.container = new ArrayList<Suitcase>();
+        this.currentWeight = 0;
+
+    }
+
+    void addSuitcase(Suitcase suitcase){
+        if((this.currentWeight + suitcase.totalWeight()) < this.maxWeight){
+            this.container.add(suitcase);
+            this.currentWeight += suitcase.totalWeight();
+
+        }
+    }
+
+    public String toString(){
+        return this.container.size()+ " suitcases " + "(" + this.currentWeight + " kg)";
+    }
+}
