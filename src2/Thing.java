6938e5914d212c4e088e44c14040f297b1b44918
Lukazenko <veikko.kiis@khk.ee>
2019-11-28 15:23:46 +0200

    part 2 ex4.1: Class Thing

---
+++ src2/Thing.java
@@ -0,0 +1,22 @@
+class Thing {
+
+    private String name;
+    private int weight;
+
+    Thing(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    int getWeight(){
+        return this.weight;
+    }
+
+    public String toString(){
+        return this.name + "(" + this.weight + " kg)";
+    }
+}
