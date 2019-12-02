5964e77cc5587020d7ca24b0c274a065a007320e
Lukazenko <veikko.kiis@khk.ee>
2019-12-02 16:03:04 +0200

    part 2 ex12.4: Purchase

---
+++ src2/Purchase.java
@@ -0,0 +1,24 @@
+class Purchase {
+    private String product;
+    private int amount;
+    private int unitPrice;
+
+    Purchase(String product, int amount, int unitPrice) {
+        this.product = product;
+        this.amount = amount;
+        this.unitPrice = unitPrice;
+    }
+
+    int price() {
+        return this.unitPrice * this.amount;
+    }
+
+    void increaseAmount() {
+        this.amount += 1;
+
+    }
+
+    public String toString() {
+        return this.product + ": " + this.amount;
+    }
+}
---
+++ src2/part2_ex12_4.java
@@ -0,0 +1,9 @@
+public class part2_ex12_4 {
+    public static void main(String[] args) {
+        Purchase purchase = new Purchase("milk", 4, 2);
+        System.out.println( "the total price of a purchase containing four milks is " + purchase.price() );
+        System.out.println( purchase );
+        purchase.increaseAmount();
+        System.out.println( purchase );
+    }
+}
